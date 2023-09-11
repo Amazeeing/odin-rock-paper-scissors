207d26f3ff9d18c4bcfac762af54f0a1834ca4da
Francesco <fplfrancesco@gmail.com>
2023-09-11 21:48:40 +0200

    Added function that plays a round

--- script.js
+++ script.js
@@ -13,6 +13,40 @@ function getComputerChoice() {
     return result;
 }
 
+function playRound(playerSelection, computerSelection) {
 
+    let playerSelectionToLowerCase = playerSelection.toLowerCase();
+    let computerSelectionToLowerCase = computerSelection.toLowerCase();
 
+    const winningPhrase = playerSelectionToLowerCase + " beat " + computerSelectionToLowerCase + "! " + "You won!";
+    const losingPhrase = playerSelectionToLowerCase + " was beat by " + computerSelectionToLowerCase + ". " + "You lost..." 
 
+    let hasPlayerWon = false;
+
+    switch (playerSelection.toLowerCase()) {
+        case "rock":
+            hasPlayerWon = computerSelection === "scissors"; 
+            break;
+
+        case "scissors":
+            hasPlayerWon = computerSelection === "paper";
+            break;
+        
+        case "paper":
+            hasPlayerWon = computerSelection === "rock";
+            break;
+    
+        default:
+            break;
+    }
+
+    if(hasPlayerWon) {
+        return winningPhrase;
+    } else {
+        return losingPhrase;
+    }
+}
+
+function game() {
+
+}
